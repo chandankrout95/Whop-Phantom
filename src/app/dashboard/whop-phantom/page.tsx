0997b7c6f2c423362e92d9c73a23bd8eacf80ea4
Shivji <shreeshivaynamstobam@gmail.com>
2025-11-28 15:36:52 +0000

    scroll able to kro

--- src/app/dashboard/whop-phantom/page.tsx
+++ src/app/dashboard/whop-phantom/page.tsx
@@ -6,9 +6,9 @@ import { WhopPhantomForm } from '@/components/dashboard/whop-phantom-form';
 
 export default function WhopPhantomPage() {
   return (
-    <div className="relative h-[calc(100vh-10rem)] overflow-hidden rounded-lg bg-black">
+    <div className="relative min-h-[calc(100vh-10rem)] rounded-lg bg-black">
       <HackerBackground />
-      <div className="relative z-10 grid h-full w-full grid-cols-1 items-start gap-4 p-4 md:grid-cols-2 md:p-8">
+      <div className="relative z-10 grid w-full grid-cols-1 items-start gap-4 p-4 md:grid-cols-2 md:p-8">
         <WhopPhantomForm />
         <PhantomDashboard />
       </div>
